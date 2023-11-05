fc0fb81ff4471477d5349acea55bee72170fb6c5
Premkumar G <133476191+premkumarg-7@users.noreply.github.com>
2023-11-05 22:38:33 +0530

    Update ExcersiceList1_P3.java

--- interview/ExcersiceList1_p3.java
+++ interview/ExcersiceList1_p3.java
@@ -1,21 +1,36 @@
 public class Main
 {
-	public static void main(String[] args) {
-	String[] names = new String[]{"A","B","C","D"};
-	int[] CS_mark = new int[]{40,98,90,90};
-	int[] English_mark = new int[] {60,94,80,28};
-	int[] Math_mark = new int[] {77,91,84,23};
-	int passmark=50;
-	int GradeA=91;
-	int GradeB=80;
-	
-	
-	int n=names.length;
-	for(int i=0;i<n;i++){
-	    if((CS_mark[i]>=GradeA ||CS_mark[i]>=GradeB)&&(English_mark[i]>=GradeA ||English_mark[i]>=GradeB)&& (Math_mark[i]>=GradeA||Math_mark[i]>=GradeB)){
-	        System.out.println(names[i]);
-	    }
-	}
-	
-	}
+  public static void main (String[]args)
+  {
+    System.out.println ("Hello World");
+
+    String[] names = new String[]
+    {
+    "A", "B", "C", "D"};
+    int[] CS_mark = new int[]{ 40, 98, 91, 90 };
+    int[] English_mark = new int[]{ 60, 94, 81, 28 };
+    int[] Math_mark = new int[]{ 77, 91, 84, 23 };
+    int passmark = 50;
+    int GradeA = 90;
+    int GradeB = 80;
+
+    int n = names.length;
+    for (int i = 0; i < n; i++)
+      {
+	if ((CS_mark[i] > GradeA && English_mark[i] > GradeA
+	     && Math_mark[i] > GradeA) || (CS_mark[i] > GradeA
+					     && English_mark[i] > GradeB
+					     && Math_mark[i] > GradeB)
+	    || (CS_mark[i] > GradeB && English_mark[i] > GradeA
+		&& Math_mark[i] > GradeB) || (CS_mark[i] > GradeB
+						&& English_mark[i] > GradeB
+						&& Math_mark[i] > GradeA))
+	  {
+	    System.out.println (names[i]);
+	  }
+      }
+  }
 }
+/*Output:
+B C
+*/
