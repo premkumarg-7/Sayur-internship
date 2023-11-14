408d6f0feee690a77ac56cd653c64726d120e67f
Premkumar G <133476191+premkumarg-7@users.noreply.github.com>
2023-11-14 18:19:49 +0530

    Create ExerciseList3_P!.java

---
+++ interview/ExerciseList3_P!.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+
+CompoundInterest();
+  }
+    public static void CompoundInterest(){
+		double salary=0.0;
+		double percentage=0.0;
+		double amount=0;
+		double interest=0.0;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the salary :");
+		salary=sc.nextDouble();
+		
+		 for (int i = 1; i <= 3; i++) {
+            System.out.printf("\nFor %d%% of your salary:\n", i * 10);
+            for (int j = 1; j <= 3; j++) {
+                System.out.printf("At %d%% interest rate: $%.2f\n", j * 5, (salary * i / 10) * Math.pow(1.0 + j * 0.05, 1));
+            }
+        }
+		
+	}
+}
+/*Output :
+Enter the salary :
+50000
+
+For 10% of your salary:
+At 5% interest rate: $5250.00
+At 10% interest rate: $5500.00
+At 15% interest rate: $5750.00
+
+For 20% of your salary:
+At 5% interest rate: $10500.00
+At 10% interest rate: $11000.00
+At 15% interest rate: $11500.00
+
+For 30% of your salary:
+At 5% interest rate: $15750.00
+At 10% interest rate: $16500.00
+At 15% interest rate: $17250.00
+*/
