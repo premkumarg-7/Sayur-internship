668ef8bf37d9abf23a996b464feebd1bb7ac2831
Premkumar G <133476191+premkumarg-7@users.noreply.github.com>
2023-11-14 18:31:12 +0530

    Create ExcerciseList3_P3.java

---
+++ interview/ExcerciseList3_P3.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class Main
+{
+  public static void main (String[]args)
+  {
+
+    int[] arr = new int[]{ 4, 3, 7, 2, 9, 1, 8 };
+      Arrays.sort (arr);
+    int max = Integer.MIN_VALUE;
+
+    for (int i:arr)
+      {
+	if (i > max)
+	  max = i;
+      }
+    System.out.println ("Max value in the array is :" + max);
+  }
+}
+
+/*
+Output:
+Max value in the array is :9
+*/
