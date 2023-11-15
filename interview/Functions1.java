a064374ea4997bdf4738fe5863c6dc611d79e0a9
Premkumar G <133476191+premkumarg-7@users.noreply.github.com>
2023-11-15 10:45:52 +0530

    Create Functions1.java

---
+++ interview/Functions1.java
@@ -0,0 +1,10 @@
+class classname{
+  static double cs_avgmarks(double cs1,double cs2,double cs3){
+    double avg=(cs1+cs2+cs3)/3;
+    return avg;
+  }
+  main method{
+  ans=cs_avgmarks(mark1,mark2,mark3);
+  print ans;
+}
+}
