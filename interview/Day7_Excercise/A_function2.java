a61abb1cbd61cf3fa53a8236f2a59e34e030b5b6
Premkumar G <133476191+premkumarg-7@users.noreply.github.com>
2023-12-06 10:14:04 +0530

    Update A_function2.java

--- interview/Day7_Excercise/A_function2.java
+++ interview/Day7_Excercise/A_function2.java
@@ -4,51 +4,34 @@
   If there is no B or 2nd B is not found, find the first C and last C and print all the letters between these two C. 
  */
 public class A_function2 { 
-	public static void main(String[] args) {
-		String s="A blind man walking on the road. An ant is ready to bite a man. A site is the safest one of the most visited site ";
+		public static void main(String[] args) {
+		String s="A blind man walking on the road. An ant is ready to bite a man. The site is the safest one of the most visited site ";
 		String[] arr=s.split(" ");
 		String result="";
-		if(!find_Aword(arr).isEmpty()) {
-			result=find_Aword(arr);
+			
+		if(!find_word(arr,"a").isEmpty()) {
+			result=find_word(arr,"a");
 			System.out.println("A between passage :"+result);
-		}else if(!find_Bwords(arr).isEmpty()) {
-			result=find_Bwords(arr);
+		}else if(!find_word(arr,"B").isEmpty()) {
+			result=find_word(arr,"B");
 			System.out.println("B between passage :"+result);
-		}else if(!find_Cwords(arr).isEmpty()) {
-			result=find_Cwords(arr);
+		}else if(!find_word(arr,"c").isEmpty()) {
+			result=find_word(arr,"c");
 			System.out.println("C between passage :"+result);
 		}
+
 	}
-	private static String find_Aword(String[] arr) {
+	private static String find_word(String[] arr, String letter) {
 		String Aword="";
 		String res="";
 		int flag=0;
 		for(String str:arr) {
-			if(str.equalsIgnoreCase("a")) {
+			if(str.equalsIgnoreCase(letter)) {
 				if(flag==1) {
 					flag=0;
 					res=Aword;
-				}else 
-					flag=1;	
-			}
-			if(flag==1) {
-				Aword+=str+" ";
-			}
-		}
-		return res;
-	}
-	private static String find_Bwords(String[] arr) {
-		String Aword="";
-		String res="";
-		int flag=0;
-		for(String str:arr) {
-			if(str.equalsIgnoreCase("b")) {
-				if(flag==1) {
-					flag=0;
-					res=Aword;
-				}else {
+				}else{ 
 					flag=1;
-					
 				}
 			}
 			if(flag==1) {
@@ -57,26 +40,7 @@ public class A_function2 {
 		}
 		return res;
 	}
-	private static String find_Cwords(String[] arr) {
-		String Aword="";
-		String res="";
-		int flag=0;
-		for(String str:arr) {
-			if(str.equalsIgnoreCase("c")) {
-				if(flag==1) {
-					flag=0;
-					res=Aword;
-				}else {
-					flag=1;
-					
-				}
-			}
-			if(flag==1) {
-				Aword+=str+" ";
-			}
-		}
-		return res;
-	}
+
 }
 /*
  * Output-
